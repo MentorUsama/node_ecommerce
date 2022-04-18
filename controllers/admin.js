dcf5ff0348d14b836e31ef5629b5ecaa5e4e1555
MentorUsama <usama.farhat.99@gmail.com>
2022-04-18 04:25:32 -0700

    database moved to sql (partial)

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,8 +14,13 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  product.save()
+    .then(()=>{
+      res.redirect('/');
+    })
+    .catch((error)=>{
+      console.log(error)
+    });
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -55,13 +60,17 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
-    });
-  });
+  Product.fetchAll()
+    .then(([rows,fieldData])=>{
+      res.render('admin/products', {
+        prods: rows,
+        pageTitle: 'Admin Products',
+        path: '/admin/products'
+      });
+    })
+    .catch(error=>{
+      console.log(error)
+    })
 };
 
 exports.postDeleteProduct = (req, res, next) => {
