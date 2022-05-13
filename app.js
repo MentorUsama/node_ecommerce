eb601a19fbe5a546c1e65fccbf148f8bf04cdf2f
MentorUsama <usama.farhat.99@gmail.com>
2022-05-13 08:20:12 -0700

    user and authentication middleware added

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequilize = require('./util/database')
+const Product = require('./models/product')
+const User = require('./models/user')
+
 
 const app = express();
 
@@ -17,14 +20,43 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req,res,next)=>{
+    User.findByPk(1)
+    .then(user => {
+        req.user = user;
+        next();
+    })
+    .catch(err => console.log(err));
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequilize.sync()
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'})
+User.hasMany(Product)
+
+sequilize
+    // .sync({force:true})
+    .sync({})
     .then(result=>{
-        app.listen(3000);
+        User.findByPk(1)
+            .then((user)=>{
+                if(!user)
+                {
+                    return User.create({name: 'Max', email: 'test@test.com'})
+                }
+                return user
+            })
+            .then(user=>{
+                console.log(user)
+                app.listen(3000);
+            })
+            .catch((user)=>{
+
+            })
+
     })
     .catch(error=>{
         console.log(error)
