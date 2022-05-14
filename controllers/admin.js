5fd07ded6879fe19dc2e378b0e0a5b76471691a3
MentorUsama <usama.farhat.99@gmail.com>
2022-05-13 19:06:48 -0700

    reverse relation for product create added

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user.createProduct({
     title:title,
     price:price,
     description:description,
@@ -33,7 +33,7 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findAll({where:{id:prodId}})
+  req.user.getProducts({where:{id:prodId}})
     .then((product)=>{
       res.render('admin/edit-product', {
         pageTitle: 'Edit Product',
@@ -71,7 +71,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.findAll().then((products)=>{
+    req.user.getProducts().then((products)=>{
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
