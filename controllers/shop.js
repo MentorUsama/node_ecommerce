d3db5b11559b14915e792775666084e311260d40
MentorUsama <usama.farhat.99@gmail.com>
2022-05-15 06:01:41 -0700

    orders page added

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
+const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
   Product.findAll().then((products)=>{
@@ -119,11 +119,43 @@ exports.postCartDeleteProduct = (req, res, next) => {
     })
 };
 
+exports.postOrder = (req, res, next) => {
+  let cartProductsGot;
+  let fetchedCart;
+  req.user.getCart()
+    .then(cart=>{
+      fetchedCart = cart;
+      return cart.getProducts()
+    })
+    .then(products=>{
+      cartProductsGot=products
+      return req.user.createOrder()
+    })
+    .then(order=>{
+      return order.addProducts(cartProductsGot.map(product=>{
+        product.orderItem={quantity:product.cartItem.quantity}
+        return product
+      }))
+    })
+    .then(result=>{
+      return fetchedCart.setProducts(null)
+    })
+    .then(result=>{
+      res.redirect('/orders')
+    })
+    .catch(error=>{})
+}
+
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
+  req.user.getOrders({include:['products']})
+    .then((orders)=>{
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders:orders
+      });
+    })  
+    .catch(error=>{})
 };
 
 exports.getCheckout = (req, res, next) => {
