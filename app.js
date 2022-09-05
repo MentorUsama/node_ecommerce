387f9eb3416b68e7246e66be29020784080bee59
unknown <muhammadusamafarhat100@gmail.com>
2022-09-05 23:39:36 +0500

    minor bug fixes

--- app.js
+++ app.js
@@ -29,7 +29,10 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  User.findById("6315f78471565b078ca09418")
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then((user) => {
       req.user = user;
       next();
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,26 @@
+const User = require('../models/user')
+
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn=req.get('Cookie').split(";")[0].trim().split('=')[1]
-  console.log(isLoggedIn)
+  const isLoggedIn = req.get("Cookie").split(";")[0].trim().split("=")[1];
+  console.log(isLoggedIn);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated:req.isLoggedIn
+    isAuthenticated: req.isLoggedIn,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  req.session.isLoggedIn=true
-  res.redirect('/') 
+  User.findById("6315f78471565b078ca09418").then((user) => {
+    req.session.isLoggedIn = true;
+    req.session.user = user;
+    res.redirect("/");
+  });
 };
 
 exports.postLogout = (req, res, next) => {
-  req.session.destroy((err)=>{
-    console.log(err)
-    res.redirect("/")
-  })
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
 };
-
